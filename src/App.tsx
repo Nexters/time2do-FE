2a8ee207ca109ee9101290fd7b20380efc27d1ea
kim-taewoo <acoustic0419@gmail.com>
2023-01-27 22:44:56 +0900

    Initial layout of main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 function App() {
   return (
-    <main className="grid min-h-screen grid-rows-header-footer">
-      <div className="bg-orange-400 h-16">HEADER</div>
-      <div className="bg-slate-300">BODY</div>
-      <div className="bg-red-400">FOOTER</div>
-    </main>
+    <div className="bg-gradient-to-br from-green-200 to-blue-400">
+      <main className="grid min-h-screen grid-rows-header-footer w-screen max-w-md m-auto">
+        <div className="bg-orange-400 h-16">HEADER</div>
+        <div className="bg-slate-300">BODY</div>
+        <div className="bg-red-400">FOOTER</div>
+      </main>
+    </div>
   )
 }
 
