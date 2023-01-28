5911032ebfdc98a6520bb885db0f321ec9198836
kim-taewoo <acoustic0419@gmail.com>
2023-01-29 02:53:46 +0900

    feat: TodoList 의 추가/삭제 간단 구현

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -4,7 +4,7 @@ interface Props {}
 
 export function DefaultLayout({}: Props) {
   return (
-    <div className="bg-gradient-to-br from-green-200 to-blue-400 text-[#191f28]">
+    <div className="bg-gradient-to-br from-green-200 to-blue-400 text-primary">
       <main className="bg-white grid min-h-screen grid-rows-header-footer relative w-screen max-w-md m-auto overflow-x-hidden">
         <Outlet />
       </main>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,18 @@
 import useCountdown from '@bradgarropy/use-countdown'
+import { useState } from 'react'
 import { TodoList } from '../components/TodoList'
 
 interface Props {}
 
 export function Home({}: Props) {
+  const [todos, setTodos] = useState([
+    { id: 1, text: '운동하기', completed: true },
+    { id: 2, text: '운동하기', completed: true },
+    { id: 3, text: '운동하기', completed: true },
+    { id: 4, text: '운동하기', completed: true },
+    { id: 5, text: '운동하기', completed: true },
+  ])
+
   const { minutes, seconds } = useCountdown({
     minutes: 30,
     seconds: 30,
@@ -13,14 +22,14 @@ export function Home({}: Props) {
 
   return (
     <>
-      <header className="w-full h-[30rem] bg-gradient-to-b from-[#BEFF7C] to-[#F9F9F9]">
+      <header className="w-full h-[30rem] bg-gradient-to-b from-lime-300 to-[#F9F9F9]">
         <div className="grid items-end justi h-full w-full">
           <div className="min-w-full text-center row-span-full col-span-full mb-[4.5rem] mb-">
             <div className="mb-5">
-              <span className="badge bg-white mx-2 py-4 text-[#191f28] border-none text-lg font-semibold px-5">
+              <span className="badge bg-white mx-2 py-4 text-primary border-none text-lg font-semibold px-5">
                 #디자인
               </span>
-              <span className="badge bg-white mx-2 py-4 text-[#191f28] border-none text-lg font-semibold px-5">
+              <span className="badge bg-white mx-2 py-4 text-primary border-none text-lg font-semibold px-5">
                 #취준
               </span>
             </div>
@@ -28,18 +37,19 @@ export function Home({}: Props) {
             <div className="mb-12">
               <div className="mb-3">
                 <span className="countdown font-mono text-5xl font-bold">
-                  <span style={{ '--value': 0 }}></span>:<span style={{ '--value': minutes }}></span>:
+                  {/* @ts-ignore */}
+                  <span style={{ '--value': 0 }}></span>:<span style={{ '--value': minutes }}></span>:{/* @ts-ignore */}
                   <span style={{ '--value': seconds }}></span>
                 </span>
               </div>
               <div className="text-lg">5:00 PM 시작 예정</div>
             </div>
-            <button className="btn rounded-[22px] bg-[#191F28] w-[180px] text-lg text-white">나가기</button>
+            <button className="btn rounded-[22px] bg-primary w-[180px] text-lg text-white">나가기</button>
           </div>
         </div>
       </header>
       <div className="bg-[#F2F4F6] py-7 px-6">
-        <TodoList />
+        <TodoList todos={todos} onChange={todos => setTodos(todos)} />
       </div>
     </>
   )
