c254f06ca94ce101b491ab0237bb4f819e90fc0c
kim-taewoo <acoustic0419@gmail.com>
2023-02-14 02:43:43 +0900

    feat: Home 페이지의 카운터 부분 컴포넌트 분리 및 스타일링 수정

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -65,7 +65,19 @@ export const TodoList = ({ todos = [], onChange }: Props) => {
   return (
     <>
       <div className="mb-4 flex items-center justify-between">
-        <h1 className="font-pretendard">1월 25일의 할 일 목록</h1>
+        <h1 className="font-pretendard text-lg">
+          할 일 목록{' '}
+          <button className="btn-circle btn h-6 w-6">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        </h1>
         <div>
           <button
             onClick={() => {
