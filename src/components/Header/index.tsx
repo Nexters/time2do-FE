dc4ac03cd31ad262e86d95f5c4a6f32ef1e8ff03
Shim MunSeong <Puterism.K@gmail.com>
2023-02-18 15:43:47 +0900

    refactor: Header 컴포넌트의 뒤로가기 버튼에 좌측 마진 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,10 +6,10 @@ interface Props {
 
 const Header = ({ title }: Props) => {
   return (
-    <header className={'mb-[2rem] flex h-14 w-full items-center text-center'}>
-      <div className={'w-full flex-1 text-center text-[1.1875rem] text-white'}>{title}</div>
-      <button className={'absolute'}>
-        <img src={BackIcon} alt={'뒤로가기 버튼'} />
+    <header className="mb-[2rem] flex h-14 w-full items-center text-center">
+      <div className="w-full flex-1 text-center text-[1.1875rem] text-white">{title}</div>
+      <button className="absolute ml-[0.625rem]">
+        <img src={BackIcon} alt="뒤로가기 버튼" />
       </button>
     </header>
   )
