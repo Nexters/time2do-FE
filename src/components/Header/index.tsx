5ca025517713508168816f52fb7be1d34608f304
Choi siun <siun0331@gmail.com>
2023-02-25 15:16:55 +0900

    feat: 뒤로가기 기능 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,16 @@
 import BackIcon from '../../assets/svg/BackIcon.svg'
+import { useNavigate } from 'react-router-dom'
 
 interface Props {
   title: string
 }
 
 const Header = ({ title }: Props) => {
+  const navigate = useNavigate()
   return (
     <header className="mb-[2rem] flex h-14 w-full items-center text-center">
       <div className="w-full flex-1 text-center text-[1.1875rem] text-white">{title}</div>
-      <button className="absolute ml-[0.625rem]">
+      <button className="absolute ml-[0.625rem]" onClick={() => navigate(-1)}>
         <img src={BackIcon} alt="뒤로가기 버튼" />
       </button>
     </header>
