82364f5dde83c1db6f83be504e9f8a0842442803
Choi siun <siun0331@gmail.com>
2023-02-28 23:54:32 +0900

    feat: 온보딩 아이템 추가, 다음 버튼 기능 구현

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -5,17 +5,25 @@ import React, { useRef, useState } from 'react'
 
 const Onboarding = () => {
   const [dotActive, setDotActive] = useState(0)
+  const [pageIndex, setPageIndex] = useState(0)
   const targetPost = useRef<HTMLDivElement>(null)
   const userInfo = localStorage.getItem('user')
   const userId = JSON.parse(userInfo as string).id
   const navigate = useNavigate()
   // eslint-disable-next-line react/jsx-key
-  const children = [<OnboardingAnimation />, <OnboardingAnimation.Second />]
+  const children = [
+    <OnboardingAnimation />,
+    <OnboardingAnimation.Second />,
+    <OnboardingAnimation.Third />,
+    <OnboardingAnimation.Forth />,
+  ]
 
   const handleScroll = (e: React.FormEvent) => {
     const currentScrollPosition = e.currentTarget.scrollLeft
-    const scrollWidth = e.currentTarget.scrollWidth / React.Children.toArray(children).length
+    const postCount = React.Children.toArray(children).length
+    const scrollWidth = e.currentTarget.scrollWidth / postCount
     setDotActive(Math.round(currentScrollPosition / scrollWidth))
+    setPageIndex(Math.round(currentScrollPosition / scrollWidth))
   }
 
   const handleClickDot = (index: number) => {
@@ -27,11 +35,22 @@ const Onboarding = () => {
   }
 
   const handleClickNext = async () => {
-    const postCount = React.Children.toArray(children).length
-    targetPost.current?.scrollTo({
-      left: (targetPost.current.scrollWidth + 1) / postCount,
-      behavior: 'smooth',
-    })
+    if (pageIndex < children.length - 1) {
+      const postCount = React.Children.toArray(children).length
+      setPageIndex(pageIndex + 1)
+      targetPost.current?.scrollTo({
+        left: ((pageIndex + 1) * targetPost.current.scrollWidth) / postCount,
+        behavior: 'smooth',
+      })
+    } else {
+      await putUser({
+        userId: userId,
+        data: {
+          onBoarding: true,
+        },
+      })
+      navigate('/')
+    }
   }
 
   const handleSkipClickEvent = async () => {
@@ -52,7 +71,8 @@ const Onboarding = () => {
           {React.Children.toArray(children).map((value, index) => (
             // eslint-disable-next-line react/jsx-key
             <div
-              className={`mr-2 h-2 w-2 rounded-3xl opacity-80 bg-${index === dotActive ? 'primary' : `[#ffffff]`}`}
+              key={`lottie${index}`}
+              className={`z-10 mr-2 h-2 w-2 rounded-3xl opacity-80 bg-${index === dotActive ? 'primary' : `white`}`}
               onClick={() => handleClickDot(index)}
             />
           ))}
@@ -69,6 +89,12 @@ const Onboarding = () => {
           </div>
           <div className="carousel-item w-full">
             <OnboardingAnimation.Second />
+          </div>
+          <div className="carousel-item w-full">
+            <OnboardingAnimation.Third />
+          </div>
+          <div className="carousel-item w-full">
+            <OnboardingAnimation.Forth />
           </div>
         </div>
       </div>
