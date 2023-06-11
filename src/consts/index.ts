9f6f39105ab4ecc6a6984fcac9eb1c6ad047a2ab
kim-taewoo <acoustic0419@gmail.com>
2023-06-11 23:39:53 +0900

    Default timer 롤백

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -5,6 +5,16 @@ export const TimerTypes = {
   COUNT_DOWN: 2,
 } as const
 
+export const defaultCountUpTimer: Timer = {
+  name: '오늘 무조건 다 끝내본다!!',
+  type: TimerTypes['COUNT_UP'],
+
+  // 클라이언트에서만 사용하거나 서버에 동기화할 때 비뀔 수 있는 필드들
+  isRunning: false,
+  id: 0,
+  makerId: 0,
+}
+
 export const BooleanNumberTypes = {
   FALSE: 0,
   TRUE: 1,
